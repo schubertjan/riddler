ce82b0eb4c29a5a736da3726b2868095a0fd39c5
schubertjan <honza.schubert@gmail.com>
2020-01-27 16:05:24 +0100

    week 3

--- scr/render.r
+++ scr/render.r
@@ -9,3 +9,8 @@ rmarkdown::render(input = "./scr/week02_2020.Rmd",
                   output_file = "week02_2020.html", 
                   output_dir = "./docs/")
 
+rmarkdown::render(input = "./scr/week03_2020.Rmd", 
+                  output_format = "html_document", 
+                  output_file = "week03_2020.html", 
+                  output_dir = "./docs/")
+
